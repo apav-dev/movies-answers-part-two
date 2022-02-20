2679c07dc9cb06a386845dce1af7793e5a99b955
Aaron Pavlick <apavlick@yext.com>
2022-02-20 15:36:50 -0500

    actor card and section

---
+++ src/components/cards/ActorCard.tsx
@@ -0,0 +1,34 @@
+import { CardProps } from "../../models/cardComponent";
+
+interface Actor {
+  name: string,
+  primaryPhoto?: {
+    image: {
+      url: string
+  }
+},
+  c_bio?: string
+}
+
+export function ActorCard(props: CardProps): JSX.Element {
+  const actor = props.result.rawData as unknown as Actor;
+
+  return (
+    <div className="min-w-1/3 h-96 relative flex flex-col justify-center" >
+      <div 
+        className="absolute z-10 h-full w-2/5 top-0 left-8 bg-cover shadow-headshot" 
+        style={{ backgroundImage: `url(${actor.primaryPhoto?.image.url})` }}>
+      </div>
+      <div className="h-80 bg-gray-100 flex justify-end text-gray-800">
+        <div className="w-1/2 flex flex-col items-center  my-8 mr-2 bg-white shadow-bio" >
+          <span className='text-3xl truncate text-ellipsis font-bold w-64 text-center font-display' >
+            {actor.name}
+          </span>
+          <div className=" text-xs overflow-y-hidden text-ellipsis w-64 text-justify font-body max-h-56 pt-4">
+            {actor.c_bio}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/sections/ActorsSection.tsx
@@ -0,0 +1,26 @@
+import { VerticalResultsDisplay } from "../components/VerticalResults";
+import { SectionComponent, SectionConfig } from "../models/sectionComponent";
+import { StandardCard } from "../components/cards/StandardCard";
+
+
+const ActorsSection: SectionComponent = function (props: SectionConfig): JSX.Element | null {
+  const { results, cardConfig, header } = props;
+  
+  if (results.length === 0) {
+    return null;
+  }
+  const cardComponent = cardConfig?.CardComponent || StandardCard;
+  
+  return (
+    <section>
+      {header}
+      <VerticalResultsDisplay
+        results={results}
+        CardComponent={cardComponent}
+        {...(cardConfig && { cardConfig })}
+        customCssClasses={{ container: 'flex overflow-x-auto space-x-12' }}
+      />
+    </section>
+  );
+}
+export default ActorsSection;
