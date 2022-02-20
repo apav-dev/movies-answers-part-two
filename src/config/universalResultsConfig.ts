f192991ca68fde7848d704b2f77acf777245bb5a
Aaron Pavlick <apavlick@yext.com>
2022-02-20 15:40:42 -0500

    adding actor card and section to config

--- src/config/universalResultsConfig.ts
+++ src/config/universalResultsConfig.ts
@@ -1,5 +1,7 @@
+import { ActorCard } from '../components/cards/ActorCard';
 import { MovieCard } from '../components/cards/MovieCard';
 import { VerticalConfig } from '../components/UniversalResults';
+import ActorsSection from '../sections/ActorsSection';
 import MoviesSection from '../sections/MoviesSection';
 
 export type UniversalResultsConfig = Record<string, VerticalConfig>;
@@ -11,5 +13,12 @@ export const universalResultsConfig: UniversalResultsConfig = {
       showOrdinal: false
     },
     SectionComponent: MoviesSection
+  },
+  actors: {
+    cardConfig: {
+      CardComponent: ActorCard,
+      showOrdinal: false
+    },
+    SectionComponent: ActorsSection
   }
 }
