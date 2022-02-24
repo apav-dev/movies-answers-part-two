f7304a16c1f4f30403ac71c5b4710c9367b591ff
Aaron Pavlick <apavlick@yext.com>
2022-02-23 23:55:34 -0500

    removing applied filters tailwind classes

--- src/pages/UniversalSearchPage.tsx
+++ src/pages/UniversalSearchPage.tsx
@@ -4,14 +4,6 @@ import { UniversalResultsConfig } from '../config/universalResultsConfig';
 import SpellCheck from '../components/SpellCheck';
 import usePageSetupEffect from '../hooks/usePageSetupEffect';
 
-// const universalResultsFilterConfig = {
-//   show: true,
-//   customCssClasses: {
-//     nlpFilter: 'border rounded-3xl px-3 py-1.5 text-sm font-medium italic text-gray-800 mr-2',
-//   },
-//   cssCompositionMethod: "assign",
-// };
-
 const directAnswerCssStyles = {
   container: 'p-4 border rounded-lg shadow-movie bg-gray-200 w-1/3', 
   fieldValueTitle: 'mb-4 text-gray-700 font-body', 
@@ -25,10 +17,15 @@ export default function UniversalSearchPage(props: { universalResultsConfig: Uni
   return (
     <div>
       <SpellCheck />
-      <DirectAnswer />
+      <DirectAnswer customCssClasses={directAnswerCssStyles} cssCompositionMethod='assign'
+      />
       <UniversalResults
         appliedFiltersConfig={{
           show: true,
+          customCssClasses: {
+            nlpFilter: 'border rounded-3xl px-3 py-1.5 text-sm font-medium italic text-white mr-2 font-display',
+          },
+          cssCompositionMethod: "assign",
         }}
         verticalConfigs={universalResultsConfig}
         cssCompositionMethod="assign"
